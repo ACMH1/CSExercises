f97301231c8caa606b7aed2983c3259a243399d7
shadowonthewind <icerimmedsteel@gmail.com>
2016-08-18 00:40:39 +0800

    Completed F3 and G4

--- CSExercises/SectionG/ExG4.cs
+++ CSExercises/SectionG/ExG4.cs
@@ -37,9 +37,96 @@ namespace CSExercises
                 55
             };
 
+            //Console.WriteLine(string.Join("\n", nameArr));
+            //Console.WriteLine(string.Join("\n", marksArr));
 
+            string[] nameSort = StringSort(nameArr);
+            int[] markSort = IntSort(marksArr);
 
+            //Console.WriteLine(string.Join("\n", nameSort));
+            //Console.WriteLine(string.Join("\n", markSort));
 
+            //Console.WriteLine(string.Join("\n", nameArr));
+            //Console.WriteLine(string.Join("\n", marksArr));
+
+            //Generate paired report sorted by name
+
+            Console.WriteLine("Name(s)\t\tMark");
+            for (int i = 0; i < nameSort.Length; i++)
+            {
+                for (int j = 0; j < nameArr.Length; j++)
+                {
+                    if (nameSort[i] == nameArr[j])
+                    {
+                        Console.WriteLine("{0}\t\t{1}", nameSort[i], marksArr[j]);
+                    }
+                }
+            }
+
+            Console.WriteLine("\n\n\n");
+
+            //Generate paired report sorted by marks
+
+            Console.WriteLine("Name\t\tMark(s)");
+            for (int i = 0; i < markSort.Length; i++)
+            {
+                for (int j = 0; j < marksArr.Length; j++)
+                {
+                    if (markSort[i] == marksArr[j])
+                    {
+                        Console.WriteLine("{0}\t\t{1}", nameArr[j], markSort[i]);
+                    }
+                }
+            }
+
+        }
+
+        public static string[] StringSort(string[] strArr)
+        {
+            string[] result = new string[strArr.Length];
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                result[i] = strArr[i];
+            }
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i].CompareTo(result[j]) > 0)
+                    {
+                        string tmp = result[i];
+                        result[i] = result[j];
+                        result[j] = tmp;
+                    }
+                }
+                //Console.WriteLine(result[i]);
+            }
+            //Console.WriteLine(result[result.Length - 1]);
+            return result;
+        }
+
+        public static int[] IntSort(int[] intArr)
+        {
+            int[] result = new int[intArr.Length];
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                result[i] = intArr[i];
+            }
+            for (int i = 0; i < result.Length - 1; i++)
+            {
+                for (int j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i].CompareTo(result[j]) < 0)
+                    {
+                        int tmp = result[i];
+                        result[i] = result[j];
+                        result[j] = tmp;
+                    }
+                }
+                //Console.WriteLine(result[i]);
+            }
+            //Console.WriteLine(result[result.Length - 1]);
+            return result;
         }
     }
 }
