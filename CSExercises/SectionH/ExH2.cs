fe383a8341ed42112a85722190cd122b316a25c9
shadowonthewind <icerimmedsteel@gmail.com>
2016-08-19 07:23:57 +0800

    Completed H1 and H2, prepared H3

--- CSExercises/SectionH/ExH2.cs
+++ CSExercises/SectionH/ExH2.cs
@@ -1,4 +1,6 @@
-﻿namespace CSExercises
+﻿using System;
+
+namespace CSExercises
 {
     public class ExH2
     {
@@ -7,14 +9,37 @@
         //indicate the starting position of the word s2 in s1.  If the word does not occur 
         //the return value should be –1.
 
+        public static void Main()
+        {
+            Console.Write("S1: ");
+            string s1 = Console.ReadLine();
+            Console.Write("S2: ");
+            string s2 = Console.ReadLine();
+
+            Console.WriteLine(FindWord(s1, s2));
+        }
+
         public static int FindWord(string s1, string s2)
         {
-            //YOUR CODE HERE
-            return 0;
+            string s1Lower = s1.ToLower();
+            string s2Lower = s2.ToLower();
 
-
-
-
+            for (int startPos = 0; startPos <= s1.Length - s2.Length; startPos++)
+            {
+                if (s1Lower[startPos] == s2Lower[0])
+                {
+                    bool stringMatch = true;
+                    for (int testPos = 1; testPos < s2.Length; testPos++)
+                    {
+                        if (s1Lower[startPos + testPos] != s2Lower[testPos])
+                            stringMatch = false;
+                        break;
+                    }
+                    if (stringMatch == true)
+                        return startPos;
+                }
+            }
+            return -1;
         }
     }
 }
--- CSExercises/SectionH/ExH3.cs
+++ CSExercises/SectionH/ExH3.cs
@@ -1,20 +1,22 @@
-﻿namespace CSExercises
+﻿using System;
+
+namespace CSExercises
 {
     public class ExH3
     {
         //Write a function (static method) that would take in an integer and 
         //return the hexadecimal.  Print out the Hex of all numbers 1 to 100 and 
         //compare your answer with that of the built in function.
+
+        public static void Main()
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Hex(num));
+        }
         public static string Hex(int i)
         {
             //YOUR CODE HERE
             return null;
-
-
-
-
-
-
         }
     }
 }
