01256dec9d957902b8425af2f0d07a00f79ed94b
ACMH1 <alex.chua.ming.hao@gmail.com>
2016-08-29 10:41:27 +0800

    Fixed bug in testPos
    testPos broke out of for loop prematurely due to missing braces.

--- CSExercises/SectionH/ExH2.cs
+++ CSExercises/SectionH/ExH2.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace CSExercises
+﻿namespace CSExercises
 {
     public class ExH2
     {
@@ -9,37 +7,14 @@ namespace CSExercises
         //indicate the starting position of the word s2 in s1.  If the word does not occur 
         //the return value should be –1.
 
-        public static void Main()
-        {
-            Console.Write("S1: ");
-            string s1 = Console.ReadLine();
-            Console.Write("S2: ");
-            string s2 = Console.ReadLine();
-
-            Console.WriteLine(FindWord(s1, s2));
-        }
-
         public static int FindWord(string s1, string s2)
         {
-            string s1Lower = s1.ToLower();
-            string s2Lower = s2.ToLower();
+            //YOUR CODE HERE
+            return 0;
 
-            for (int startPos = 0; startPos <= s1.Length - s2.Length; startPos++)
-            {
-                if (s1Lower[startPos] == s2Lower[0])
-                {
-                    bool stringMatch = true;
-                    for (int testPos = 1; testPos < s2.Length; testPos++)
-                    {
-                        if (s1Lower[startPos + testPos] != s2Lower[testPos])
-                            stringMatch = false;
-                        break;
-                    }
-                    if (stringMatch == true)
-                        return startPos;
-                }
-            }
-            return -1;
+
+
+
         }
     }
 }
